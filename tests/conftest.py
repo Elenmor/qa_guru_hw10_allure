2b026ce5206b6e905a259519877061511e7b3e82
Elena Morilova <elena.morilova@mail.ru>
2023-05-24 13:35:00 +0400

    commit changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
-from selene import browser
+from selene.support.shared import browser
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.driver.set_window_size(1920, 1080)
-
+def browser_start():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.base_url = 'https://github.com'
     yield
     browser.quit()
-
